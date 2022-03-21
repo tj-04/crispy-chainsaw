78630f2797d4166ae66912d1dcf6a2295fc075c5
tj-04 <tejas@evergreenclub.in>
2022-03-21 18:26:41 +0530

    Testing circle ci cd

--- routes.js
+++ routes.js
@@ -1,13 +1,7 @@
 'use strict';
 
-const cM = require('./controllers/implio-fileupload');
-const cM1 = require('./controllers/implio1');
-
 exports.init = (router) => {
     router.route('/').get((req, res) => {
-      res.send('Implio Dashboard');
+      res.send('Circle CI/ CD Dashboard');
     });
-
-    router.route('/bd').post(cM.createPost);
-    router.route('/b1').post(cM1.createPost);
 }
